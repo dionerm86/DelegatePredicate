1160eb59dd5beed14f07f5c2d4071c49c3acdb56
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-23 14:34:17 -0300

    Exemplo de Action com ForEach

--- PredicateDelegate/Program.cs
+++ PredicateDelegate/Program.cs
@@ -15,21 +15,32 @@ namespace PredicateDelegate
             list.Add(new Product("Xbox One X", 2200.00));
             list.Add(new Product("PS4", 2000.00));
 
-            //list.RemoveAll(p => p.Price >= 1900.00);
-            list.RemoveAll(ProductTest);
+            
+            //list.RemoveAll(p => p.Price >= 1900.00); Exemplo com lambida
+            list.RemoveAll(ProductTest);//Remover prod com preço > que 1900.00
 
+            list.ForEach(UpdateProduct);// Exibir o produtos com 10% de acréscimo.
+            /*Outras forma de chamar a função UpdateProduct
+            Action<Product> act = UpdateProduct;
+            list.ForEach(act);
+            Action(Product> act = p => {p.Price =+ p.Price * 0.1};
+            */
+            //Exibir o PredicateDelegate
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
-
             }
-
             Console.ReadLine();
         }
 
         public static bool ProductTest(Product p)
         {
             return p.Price >= 1900.00;
+        }
+
+        static void UpdateProduct(Product p)
+        {
+            p.Price += p.Price * 0.1;
         }
     }
 }
