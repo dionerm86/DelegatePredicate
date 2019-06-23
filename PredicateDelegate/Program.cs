4b41ec0853e8fb3861a9220d4ba0d23ff0f8b5ac
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-23 15:22:34 -0300

    Exemplo de FuncDelegate com Select

--- PredicateDelegate/Program.cs
+++ PredicateDelegate/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using PredicateDelegate.Entities;
-
+using System.Linq;
 namespace PredicateDelegate
 {
     class Program
@@ -10,37 +10,78 @@ namespace PredicateDelegate
         {
             List<Product> list = new List<Product>();
 
-            list.Add(new Product("TV LG 4K", 1550.00));
+            list.Add(new Product("Tv Lg 4K", 1550.00));
             list.Add(new Product("PocoPhone F1", 1800.00));
             list.Add(new Product("Xbox One X", 2200.00));
             list.Add(new Product("PS4", 2000.00));
 
-            
-            //list.RemoveAll(p => p.Price >= 1900.00); Exemplo com lambida
+            //PredicateDelegate
             list.RemoveAll(ProductTest);//Remover prod com preço > que 1900.00
+                                        //list.RemoveAll(p => p.Price >= 1900.00); Exemplo com lambida
 
-            list.ForEach(UpdateProduct);// Exibir o produtos com 10% de acréscimo.
-            /*Outras forma de chamar a função UpdateProduct
-            Action<Product> act = UpdateProduct;
-            list.ForEach(act);
-            Action(Product> act = p => {p.Price =+ p.Price * 0.1};
-            */
-            //Exibir o PredicateDelegate
+            Console.WriteLine("Predicate Delegate:");
+
             foreach (Product p in list)
             {
                 Console.WriteLine(p);
             }
-            Console.ReadLine();
-        }
 
+            Console.WriteLine();
+
+            //ActionDelegate
+            list.ForEach(UpdateProduct);// Exibir o produtos com 10% de acréscimo.
+
+            Console.WriteLine("Action Delegate:");
+
+            foreach (Product p1 in list)
+            {
+                Console.WriteLine(p1);
+            }
+
+            Console.WriteLine();
+
+            /*
+                Outras forma de chamar a função UpdateProduct
+                
+                Action<Product> act = UpdateProduct;
+                list.ForEach(act);
+
+                Action(Product> act = p => {p.Price =+ p.Price * 0.1};
+
+                list.ForEach(p => {p.Price =+ p.Price * 0.1});
+            */
+
+            //Exemplo de FuncDelegate
+
+            List<string> list2 = list.Select(NameUpper).ToList();//Selecionar os elementos da lista e exibir em ToUpper
+
+            Console.WriteLine("Func Delegate:");
+
+            foreach (string s in list2)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.ReadLine();
+
+
+        }
+        //Função para retornar o PredicateDelegate
         public static bool ProductTest(Product p)
         {
             return p.Price >= 1900.00;
         }
 
+        //Função para retornar o ActionDelegate
         static void UpdateProduct(Product p)
         {
             p.Price += p.Price * 0.1;
+        }
+
+        //Função para retornar o SelectDelegate
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
         }
     }
 }
