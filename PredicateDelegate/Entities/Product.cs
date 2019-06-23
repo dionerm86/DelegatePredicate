997a46eafe532c24353e28d96f3b0ac10448c67a
Dione Regis Maciel <dionerm86@gmail.com>
2019-06-23 14:08:15 -0300

    Exemplo de PredicateDelegate com o RemoveAll();

---
+++ PredicateDelegate/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿
+namespace PredicateDelegate.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return "Product: " + Name + "\nPrice: " + Price.ToString("F2");
+        }
+    }
+}
--- PredicateDelegate/Program.cs
+++ PredicateDelegate/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using PredicateDelegate.Entities;
 
 namespace PredicateDelegate
 {
@@ -6,7 +8,28 @@ namespace PredicateDelegate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("TV LG 4K", 1550.00));
+            list.Add(new Product("PocoPhone F1", 1800.00));
+            list.Add(new Product("Xbox One X", 2200.00));
+            list.Add(new Product("PS4", 2000.00));
+
+            //list.RemoveAll(p => p.Price >= 1900.00);
+            list.RemoveAll(ProductTest);
+
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+
+            }
+
+            Console.ReadLine();
+        }
+
+        public static bool ProductTest(Product p)
+        {
+            return p.Price >= 1900.00;
         }
     }
 }
